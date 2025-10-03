27ccd5b250874873be4270544759e99df8cf75c1
nemnem202 <naimelhabbas@gmail.com>
2025-10-03 10:40:02 +0200

    finished card-component

--- src/ui/partials/movie_card.tsx
+++ src/ui/partials/movie_card.tsx
@@ -4,9 +4,22 @@ import "../styles/partials/movie_card.css";
 export default function MovieCard() {
   return (
     <div className="card-container">
-      <img src="" alt="" className="card-img" />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Transformers_One_Official_Poster.jpg/250px-Transformers_One_Official_Poster.jpg"
+        alt=""
+        className="card-img"
+      />
       <div className="card-content">
-        <h1>Stranformers</h1>
+        <p className="title">Stranformers</p>
+        <div>
+          <p className="description">
+            Jack Reacher must uncover the truth behind a major government conspiracy in order to
+            clear his name. On the run as a fugitive from the law, Reacher uncovers a potential
+            secret from his past that could change his life forever.
+          </p>
+          <p className="date">01/02/2017</p>
+        </div>
+
         <Rating rate={25.6} size={1} />
       </div>
     </div>
