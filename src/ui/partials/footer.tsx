098cea32292a265d797ffb049df172117ca8c802
nemnem202 <naimelhabbas@gmail.com>
2025-10-04 19:44:30 +0200

    added footer

--- src/ui/partials/footer.tsx
+++ src/ui/partials/footer.tsx
@@ -1,3 +1,15 @@
+import "../styles/partials/footer.css";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="app_footer">
+      <div className="footer_bottom">
+        <p>© {new Date().getFullYear()} MyMovieApp. Tous droits réservés.</p>
+        <p>
+          <a href="/mentions-legales">Mentions légales</a> |{" "}
+          <a href="/confidentialite">Politique de confidentialité</a>
+        </p>
+      </div>
+    </footer>
+  );
 }
