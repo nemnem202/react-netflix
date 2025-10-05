0b61316b34d78ff9164a433dea9f43d3e9d2cd33
nemnem202 <naimelhabbas@gmail.com>
2025-10-05 17:23:05 +0200

    again, fixed caroussel behavior

--- src/ui/partials/caroussel.tsx
+++ src/ui/partials/caroussel.tsx
@@ -99,7 +99,7 @@ export default function Caroussel({
             height="5rem"
             viewBox="0 -960 960 960"
             onClick={movePrev}
-            // onMouseEnter={() => handlePrevEnter()}
+            onMouseEnter={() => handlePrevEnter()}
             // onMouseLeave={() => handleNextEnter(0.2)}
           >
             <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
